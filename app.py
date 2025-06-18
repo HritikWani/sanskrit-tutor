84cc475b066069263e272b4e288f2ab2cf75333b
HritikWani <wanihritik@gmail.com>
2025-06-18 23:14:35 +0530

    Added admin delete features and schedule form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template
 from pymongo import MongoClient
 from werkzeug.utils import secure_filename
 import os
@@ -7,36 +7,82 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
 # MongoDB Connection
-MONGO_URI = "mongodb+srv://<username>:<password>@cluster0.mongodb.net/sanskrit_tuition?retryWrites=true&w=majority"
+MONGO_URI = os.environ.get("MONGO_URI")
 client = MongoClient(MONGO_URI)
 db = client['sanskrit_tuition']
 students_col = db['students']
 tests_col = db['tests']
+schedules_col = db['schedules']
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
-def upload_test():
-    file = request.files['file']
-    student_id = request.form['student_id']
-    subject = request.form['subject']
-    test_date = request.form['test_date']
-    marks = request.form['marks']
+@app.route('/admin')
+def admin_page():
+    return render_template('admin.html')
 
-    filename = secure_filename(file.filename)
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+@app.route('/admin/students')
+def view_students():
+    students = list(students_col.find())
+    return render_template('students.html', students=students)
 
-    tests_col.insert_one({
-        'student_id': student_id,
-        'subject': subject,
-        'test_date': test_date,
-        'marks': marks,
-        'pdf_file': filename
-    })
+@app.route('/admin/tests')
+def view_tests():
+    tests = list(tests_col.find())
+    return render_template('tests.html', tests=tests)
 
-    return "Test uploaded successfully."
+@app.route('/admin/schedules')
+def view_schedules():
+    schedules = list(schedules_col.find())
+    return render_template('schedules.html', schedules=schedules)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+from flask import request, redirect
+
+@app.route('/admin/add-student', methods=['GET', 'POST'])
+def add_student():
+    if request.method == 'POST':
+        student_data = {
+            "student_id": request.form['student_id'],
+            "name": request.form['name'],
+            "class": int(request.form['class']),
+            "school": request.form['school'],
+            "contact": request.form['contact']
+        }
+        students_col.insert_one(student_data)
+        return redirect('/admin/students')  # redirect after adding
+
+    return render_template('add_student.html')
+
+@app.route('/admin/add-schedule', methods=['GET', 'POST'])
+def add_schedule():
+    if request.method == 'POST':
+        schedule_data = {
+            "class": int(request.form['class']),
+            "school": request.form['school'],
+            "day": request.form['day'],
+            "time": request.form['time']
+        }
+        schedules_col.insert_one(schedule_data)
+        return redirect('/admin/schedules')
+    
+    return render_template('add_schedule.html')
+
+from bson.objectid import ObjectId
+
+@app.route('/admin/delete-student/<id>')
+def delete_student(id):
+    students_col.delete_one({"_id": ObjectId(id)})
+    return redirect('/admin/students')
+
+@app.route('/admin/delete-test/<id>')
+def delete_test(id):
+    tests_col.delete_one({"_id": ObjectId(id)})
+    return redirect('/admin/tests')
+
+@app.route('/admin/delete-schedule/<id>')
+def delete_schedule(id):
+    schedules_col.delete_one({"_id": ObjectId(id)})
+    return redirect('/admin/schedules')
+
